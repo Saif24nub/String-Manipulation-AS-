6f6ebb091b3b70d0d96f6e36cc045601c693f4f0
Saif24nub <97380829+Saif24nub@users.noreply.github.com>
2022-01-12 13:19:20 +0500

    Create Donuts.vb

---
+++ Donuts.vb
@@ -0,0 +1,24 @@
+Module Module1
+
+    Sub Main()
+
+        Dim DONUTS As Integer
+
+        DONUTS = 0
+
+        Console.WriteLine("ENTER NUMBER OF DONUTS:")
+        DONUTS = Console.ReadLine
+
+        If DONUTS >= 10 Then
+            Console.WriteLine("MANY")
+        Else
+            Console.WriteLine("NUMBER OF DONUTS:" & DONUTS)
+
+        End If
+
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
